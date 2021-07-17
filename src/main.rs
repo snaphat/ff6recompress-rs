0ce70005e139f0c5e0207648fd1666d4382e4727
Aaron Landwehr <snaphat@gmail.com>
2021-07-16 23:04:43 -0400

    - Make unpack_lzss() safe. - Update apultra-rs. - Change Box<Error> for aplib_decompress and aplib_compress to Impl Error.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+extern crate apultra;
+use apultra::DecompressionError;
+use std::cell::RefCell;
 use std::{error::Error, fs, io};
-
 fn conv_addr(addr: usize) -> usize {
     if addr & 0x408000 != 0 {
         addr & 0x3FFFFF
@@ -19,71 +21,82 @@ fn open(path: &str) -> Result<Vec<u8>, io::Error> {
     //file.read_to_string(&mut contents)?;
 }
 
-fn unpack_lzss(input: &[u8]) -> (Vec<u8>, usize) {
-    // Original decode:
-    let src = input;
-    let mut dest: Vec<u8> = Vec::new();
-    let mut s: usize = 0; // source pointer
-    let mut buffer: Vec<u8> = vec![0; 0x800];
-    let mut b = 0x07DE;
-    let mut line: Vec<u8> = vec![0; 34];
-    let mut length: usize = src[s] as usize;
-    s += 1;
-    length |= (src[s] as usize) << 8;
-    s += 1;
-    if length == 0 {
-        return (dest, 0);
-    }
+fn unpack_lzss(input: &[u8]) -> Result<(Vec<u8>, usize), DecompressionError> {
 
-    while s < length {
+    if input.len() < 2 { return Err(DecompressionError); }
+
+    // Get length of compressed data.
+    let length = input[0] as usize | (input[1] as usize) << 8;
+
+    // Check if length valid.
+    if length == 0 {
+        return Err(DecompressionError);
+    };
+
+    // Get slice of data of the exact length (for OoB handling).
+    let mut src = input[2..length+2].iter();
+    let s = RefCell::new(2); // Source index.
+
+    // Smart wrapper for iterator. Returns DecompressionError if iterating past the end of the buffer.
+    let mut next = || -> Result<u8, DecompressionError> {
+        src.next().ok_or(DecompressionError).map(|val| {
+            *s.borrow_mut() += 1; // Update source index.
+            *val // Return the next value.
+        })
+    };
+
+    // allocate intermediate buffer starting at index 0x07DE (ff6 start).
+    let mut buffer: [u8; 0x800] = [0; 0x800];
+    let mut b = 0x07DE;
+
+    // Reserve space for decompressed data.
+    let mut dest: Vec<u8> = Vec::new();
+    dest.reserve_exact(length);
+
+    // Decompress data.
+    while *s.borrow() < length {
         // read header
-        let mut header = src[s];
-        s += 1;
+        let mut header = next()?;
 
         for _pass in 0..8 {
-            let mut l = 0;
+            let mut line: Vec<u8> = Vec::new();
+
             if header & 1 != 0 {
                 // single byte (uncompressed)
-                let c = src[s];
-                s += 1;
-                line[l] = c;
-                l += 1;
+                let c = next()?;
+                line.push(c);
                 buffer[b] = c;
-                b += 1;
-                b &= 0x07FF;
+                b = b + 1 & 0x7FF;
             } else {
                 // 2-bytes (compressed)
-                let mut w: usize = src[s] as usize;
-                s += 1;
-                w |= (src[s] as usize) << 8;
-                s += 1;
+                let mut w = next()? as usize;
+                w |= (next()? as usize) << 8;
                 let r = (w >> 11) + 3;
                 w &= 0x07FF;
 
                 for i in 0..r {
                     let c = buffer[(w + i) & 0x07FF];
-                    line[l] = c;
-                    l += 1;
+                    line.push(c);
                     buffer[b] = c;
-                    b += 1;
-                    b &= 0x07FF;
+                    b = b + 1 & 0x7FF;
                 }
             }
             // copy this pass to the destination buffer
-            dest.extend_from_slice(&line[..l]);
+            dest.append(&mut line);
 
-            // reached end of compressed data
-            if s >= length {
+            // Break if we're at the end of the compressed data.
+            if *s.borrow() == length {
                 break;
-            }
+            };
+
             header >>= 1;
         }
     }
 
-    return (dest, s); // (decompressed data, original compressed size)
+    return Ok((dest, length)); // (decompressed data, original compressed size)
 }
 
-fn aplib_compress(input: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+fn aplib_compress(input: &[u8]) -> Result<Vec<u8>, impl Error> {
     let window_size = 1024;
     let dictionary_size = 0;
     let flags = 0;
@@ -92,7 +105,7 @@ fn aplib_compress(input: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
     apultra::compress(input, window_size, dictionary_size, flags, progress, stats)
 }
 
-fn aplib_decompress(input: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+fn aplib_decompress(input: &[u8]) -> Result<Vec<u8>, impl Error> {
     let dictionary_size = 0;
     let flags = 0;
     apultra::decompress(input, dictionary_size, flags)
@@ -101,7 +114,7 @@ fn aplib_decompress(input: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
 impl Rom {
     fn _recompress(self, offset: usize) {
         let offset = conv_addr(offset);
-        let (uncompressed, compressed_size) = unpack_lzss(&self.rom[offset..]);
+        let (uncompressed, compressed_size) = unpack_lzss(&self.rom[offset..]).unwrap();
         let a = aplib_compress(&uncompressed);
         let a = a.unwrap();
         let b = aplib_decompress(&a);
@@ -109,7 +122,14 @@ impl Rom {
         let c = aplib_compress(&b);
         let c = c.unwrap();
 
-        println!("{}\n{}\n{}\n{}\n", compressed_size, a.len(), b.len(), c.len());
+        println!(
+            "{}\n{}\n{}\n{}\n{}\n",
+            uncompressed.len(),
+            compressed_size,
+            a.len(),
+            b.len(),
+            c.len()
+        );
         assert_eq!(a, c);
         //auto [unpacked, old_size] = unpack_lzss(&rom[offset]);
         //auto repacked             = pack_aplib(unpacked, 0x10000);
