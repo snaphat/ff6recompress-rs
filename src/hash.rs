9701feb077f6f76cb60e4557093c2fedcab42bea
Aaron Landwehr <snaphat@gmail.com>
2021-08-21 19:00:23 -0400

    Add hash_one unit tests

--- src/hash.rs
+++ src/hash.rs
@@ -12,3 +12,25 @@ pub trait HashOne: Hash
     }
 }
 impl HashOne for [u8] {}
+
+#[cfg(test)] #[rustfmt::skip]
+mod test
+{
+    use super::HashOne;
+
+    #[test]
+    fn hash_one_eq()
+    {
+        let arr0: [u8; 257] = [0;257];
+        let arr1: [u8; 257] = [0;257];
+        assert_eq!(arr0.hash_one(), arr1.hash_one());
+    }
+
+    #[test]
+    fn hash_one_ne()
+    {
+        let arr0: [u8; 254] = [0;254];
+        let arr1: [u8; 256] = [0;256];
+        assert_ne!(arr0.hash_one(), arr1.hash_one());
+    }
+}
